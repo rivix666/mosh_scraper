601cc342b9b9068a332a38e6ce7450725ddd4545
rivix666 <rivix666@users.noreply.github.com>
2020-08-22 13:39:09 +0200

    Add sections directories
    From now on script should create folders for every section in download directory

--- mosh_scrap.py
+++ mosh_scrap.py
@@ -1,4 +1,12 @@
-"""TODO"""
+"""
+    Main Mosh-Scrap module
+
+    If you want to use it, just create MoshScraper object with url to course as first argument
+    and EBrowser type as second. Ensure you have sign in in chosen browser to mosh site, and use "download" method.
+
+    This script will copy cookies from the browser you choose and then download all videos from course as logged user.
+
+"""
 
 import requests
 from enum import Enum
@@ -9,29 +17,36 @@ from utils.progress_bar import ProgressBar
 
 
 class EBrowser(Enum):
-    """TODO"""
+    """ Enum used to identify browser arguments"""
     Firefox = "-f"
     Chrome = "-c"
 
 
-def ensure_download_dir_exists():
-    pth = Path("download")
+def ensure_dir_exists(directory):
+    pth = Path(directory)
     if not pth.exists():
         pth.mkdir()
 
 
-def download_video(v_data):
+def download_video(v_data, directory):
     if not v_data:
         return False
     r = requests.get(v_data["url"], stream=True)
     file_name = v_data["name"]
     indicator = ProgressBar(int(r.headers.get('content-length')))
-    with open("download/" + file_name, 'wb') as f:
+    with open(directory + file_name, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024 * 1024):
             if chunk:
                 f.write(chunk)
                 indicator.progress += len(chunk)
     return True
+
+
+def count_urls(urls_data):
+    count = 0
+    for data in urls_data:
+        count += len(data["urls"])
+    return count;
 
 
 class MoshScraper:
@@ -48,45 +63,64 @@ class MoshScraper:
     def download(self):
         """TODO"""
         print("---------------- DOWNLOAD BEGIN ----------------")
-        urls = self.get_items_urls()
+        urls = self.get_items_data()
         self.download_videos(urls)
         print("---------------- DOWNLOAD END ----------------")
 
-    def get_items_urls(self):
-        try:
-            response = self.session.get(self.course_url)
-        except requests.exceptions.RequestException as e:
-            print("Exception:", e)
-            return
-        soup = BeautifulSoup(response.text, "html.parser")
-        items = soup.select(".section-item")
-        return [self.base_url + it.select_one("a.item")["href"] for it in items]
+    def get_items_data(self):
+        response = self.request_response(self.course_url)
+        if response:
+            urls = []
+            soup = BeautifulSoup(response.text, "html.parser")
+            sections = soup.select(".course-section")
+            for sec in sections:
+                # Prepare section title
+                title = sec.select_one(".section-title")
+                # Remove unnecessary data from it
+                title.select_one(".section-days-to-drip").decompose()
+                # Get URLs
+                items = sec.select(".section-item")
+                urls.append({"title": title.getText().strip(),
+                             "urls": [self.base_url + it.select_one("a.item")["href"] for it in items]})
+            return urls;
 
     def get_video_data(self, video_url):
-        try:
-            response = self.session.get(video_url)
-        except requests.exceptions.RequestException as e:
-            print("Exception:", e)
-            return
-        soup = BeautifulSoup(response.text, "html.parser")
-        video_url = soup.select_one("a.download")["href"]
-        video_name = soup.select_one("a.download")["data-x-origin-download-name"]
-        return {"name": video_name, "url": video_url}
+        response = self.request_response(video_url)
+        if response:
+            soup = BeautifulSoup(response.text, "html.parser")
+            video_url = soup.select_one("a.download")["href"]
+            video_name = soup.select_one("a.download")["data-x-origin-download-name"]
+            return {"name": video_name, "url": video_url}
 
     def download_videos(self, urls):
-        ensure_download_dir_exists()
-        urls_num = len(urls)
-        success = failure = 0
-        for index, url in enumerate(urls):
-            v_data = self.get_video_data(url)
-            if download_video(v_data):
-                file_name = v_data["name"]
-                success += 1
-                print(f" {index + 1}/{urls_num}: \"{file_name}\" - Downloaded")
-            else:
-                file_name = v_data["name"]
-                failure += 1
-                print(f" {index + 1}/{urls_num}: \"{file_name}\" - Can not download")
+        ensure_dir_exists("download")
+        urls_num = count_urls(urls)
+        success = failure = prev = 0
+        for data in urls:
+            section_directory = "download/" + data["title"]
+            ensure_dir_exists(section_directory)
+            for index, url in enumerate(data["urls"]):
+                try:
+                    v_data = self.get_video_data(url)
+                except TypeError:
+                    failure += 1
+                    print(f" {prev + index + 1}/{urls_num}: Not a video")
+                else:
+                    if download_video(v_data, section_directory + "/"):
+                        file_name = v_data["name"]
+                        success += 1
+                        print(f" {prev + index + 1}/{urls_num}: \"{file_name}\" - Downloaded")
+                    else:
+                        file_name = v_data["name"]
+                        failure += 1
+                        print(f" {prev + index + 1}/{urls_num}: \"{file_name}\" - Can not download")
+            prev += len(data["urls"])
+
+    def request_response(self, url):
+        try:
+            return self.session.get(url)
+        except requests.exceptions.RequestException as e:
+            print("Exception:", e)
 
     @property
     def base_url(self):
